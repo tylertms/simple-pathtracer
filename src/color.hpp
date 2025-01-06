4dfe3a4da077fc36b775b75a7a3572f96aff93dc
Tyler Smith <tylertms@icloud.com>
2025-01-05 21:32:35 -0500

    clamp colors

--- src/color.hpp
+++ src/color.hpp
@@ -1,9 +1,10 @@
 #ifndef COLOR_HPP
 #define COLOR_HPP
 
+#include <iostream>
 #include "vec3.hpp"
 
-#include <iostream>
+using namespace std;
 typedef vec3 color;
 
 void write_color(std::ostream& out, const color& pixel_color) {
@@ -11,9 +12,9 @@ void write_color(std::ostream& out, const color& pixel_color) {
   auto g = pixel_color.y();
   auto b = pixel_color.z();
 
-  unsigned char r_num = 255.999 * r;
-  unsigned char g_num = 255.999 * g;
-  unsigned char b_num = 255.999 * b;
+  unsigned char r_num = clamp(255.999 * r, 0.0, 255.0);
+  unsigned char g_num = clamp(255.999 * g, 0.0, 255.0);
+  unsigned char b_num = clamp(255.999 * b, 0.0, 255.0);
 
   out << r_num << g_num << b_num;
 }
